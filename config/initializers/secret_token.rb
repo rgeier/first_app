ea007aaa0fabf913e70124c72fb2dbf0785f687a
Richard Geier <richard@pictlabs.com>
2012-03-19 00:35:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd2a29e62daa70091818a2d49857adc35b5f8ae53843c56fffa69b1945e418ea69c578f4047bd279e31c13f537ae3625c2c2477b092949ee2d2d0f4de78c77319'
